20e45f426d0e13c137d271b1933626ec1cb00b9d
neiroset-7373 <2015nikmayboroda2015@bk.ru>
2026-05-30 13:25:30 +0300

    Add Capacitor configuration for mobile app

---
+++ client/version 0.1.0-alpha/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.wintozo.wintoblox',
+  appName: 'WintoBlox - Alpha Version',
+  webDir: 'static'
+};
+
+export default config;
